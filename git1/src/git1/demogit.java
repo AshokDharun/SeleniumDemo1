02f02ad7bd390960650c88e66013ef3a55ac6b83
AshokDharun <ashoksekars43@gmail.com>
2023-06-06 10:23:01 +0530

    1 to 100

--- git1/src/git1/demogit.java
+++ git1/src/git1/demogit.java
@@ -4,5 +4,16 @@ public class demogit
 {
 public static void main(String[] args) {
 	System.out.println("this is demo");
+	printnom(1,100);
+}
+
+private static void printnom(int i, int j) {
+	if(i<j)
+	{ 
+		System.out.print(i+ " ");
+		i=i+1;
+		printnom(i, j);
+	}
+	
 }
 }
